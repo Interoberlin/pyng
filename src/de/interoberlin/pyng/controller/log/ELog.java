6ed38dc72c9470ea6ae625dd64e59b1fba1ea671
Florian <florian.schwanz@interoberlin.de>
2014-10-26 20:11:05 +0100

    Added task uploadArchives

--- src/de/interoberlin/pyng/controller/log/ELog.java
+++
@@ -1,6 +0,0 @@
-package de.interoberlin.pyng.controller.log;
-
-public enum ELog
-{
-	TRACE, DEBUG, INFO, WARN, ERROR, FATAL,
-}
--- src/de/interoberlin/pyng/model/sound/ESound.java
+++
@@ -1,6 +0,0 @@
-package de.interoberlin.pyng.model.sound;
-
-public enum ESound
-{
-    BING, BONG, GAME_OVER
-}
